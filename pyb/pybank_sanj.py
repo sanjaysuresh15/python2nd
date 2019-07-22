805686a610762ab42f7447244f1245756213dc66
Sanjay Suresh/JAM/Brenntag <sanjaysuresh15@gmail.com>
2019-07-21 21:19:09 -0500

    add files

--- pyb/pybank_sanj.py
+++ pyb/pybank_sanj.py
@@ -1,4 +1,6 @@
-import os
-import csv
-csvfile = os.path.join('..','Resources','budget_data.csv',)
-print(csvfile)
+
+
+
+   
+
+
---
+++ pyp/pypoll_sanj.py
@@ -0,0 +1,9 @@
+import os
+import csv
+csvpath = os.path.join('python2nd','resources','election_data.csv')
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)

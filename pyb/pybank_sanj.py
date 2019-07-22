7c550f163c92008b44a27f5801b8ce720eaa8bb7
Sanjay Suresh/JAM/Brenntag <sanjaysuresh15@gmail.com>
2019-07-21 21:39:37 -0500

    add files

--- pyb/pybank_sanj.py
+++ pyb/pybank_sanj.py
@@ -1,4 +1,12 @@
+import os
+import csv
+csvpath = os.path.join('python2nd','resources','budget_data.csv')
+with open(csvpath, newline='') as csvfile:
 
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
 
 
    

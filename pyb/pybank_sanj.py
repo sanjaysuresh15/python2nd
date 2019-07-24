3bc308b88c413e0dfdcd438700dfb341388210dc
Sanjay Suresh/JAM/Brenntag <sanjaysuresh15@gmail.com>
2019-07-23 22:44:51 -0500

    add

--- pyb/pybank_sanj.py
+++ pyb/pybank_sanj.py
@@ -1,29 +1,34 @@
 import os
 import csv
-budget_csv = os.path.join(".", "Resources", "budget_data.csv")
+budget_csv = os.path.join(".", "resources", "budget_data.csv")
  
 # Open and read csv
 total = 0
 with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    # number of months and dollars
+    #define variables
     month = []
-    
+    month_list = month
     money_sum = 0
     change = 0
+    
+    def percentChange():
+        float((row+1)-row)/abs(row)*100.00
+    
     csv_header = next(csvfile)
     for row in csvreader:
         month.append(row[0])
         money_sum = money_sum + int(row[1])
         
-    month_list = month
-    
+    # number of months and dollars
+
     print("Total Months: " + str(len(month_list)))  
     print("Total Dollars: " + str(money_sum))  
+    
+    
     for row in csvreader:
-        def percentChange():
-          change =  return((float((row+1)-row)/abs(row)))*100.00
-    print(change)
+      change + percentChange()
+    print("Percent Change: " + str(change))        
 
         
 
